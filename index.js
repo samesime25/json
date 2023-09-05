ad17ee722af39fc2d17d7feb4cd90d380b13a6fe
samesime25 <samesime25@gmail.com>
2023-09-05 01:27:52 +0100

    my json file

---
+++ index.js
@@ -0,0 +1,27 @@
+ const click = document.querySelector(".button")
+ const text = document.querySelector(".Text")
+ const you = document.querySelector('.you')
+ click.addEventListener('click', ()=>{
+   const fine =you.innerHTML= text.value;
+   bee()
+ }
+)
+  function bee() {
+    click.forEach(fine => {
+      let html= document.createElement('p')
+      html = `<p>${click}</p>`;
+      html.appendChild.body
+     });
+  }
+  
+ /*Name={
+Allen: "Do", Age: 8
+ }
+ const covert=JSON.stringify(Name);
+
+ console.log(JSON.parse(covert));
+
+ localStorage.setItem('message', 'hello')
+
+ console.log(localStorage.getItem('message'));*/
+ 
